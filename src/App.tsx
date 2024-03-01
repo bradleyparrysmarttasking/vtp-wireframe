65921a99fc63120a10a01b74313f2784d5d762e1
Bradley Parry <bradleyparry@Bradleys-MacBook-Pro.local>
2024-03-01 10:01:11 +0100

    Visual updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-// import { useState } from "react";
-
-// import "./App.css";
 import { useState } from "react";
-// import Heading from "./design_system/heading/src";
-// import Main from "./design_system/main/src";
 import {
   Main,
   Table,
@@ -13,10 +8,11 @@ import {
   GridCol,
   TopNav,
   Heading,
+  SearchBox,
+  Link,
+  PhaseBanner,
+  Footer,
 } from "govuk-react";
-// import SearchBox from "./design_system/search-box/src";
-
-// import TopNav from "./design_system/top-nav/src";
 
 type Victim = {
   fullName: string;
@@ -31,36 +27,40 @@ function App() {
       <TopNav
         company={
           <TopNav.Anchor href="https://example.com" target="new">
-            <TopNav.IconTitle>CPS Test</TopNav.IconTitle>
+            <TopNav.IconTitle>CPS</TopNav.IconTitle>
           </TopNav.Anchor>
         }
         serviceTitle="Victim Centric View"
       />
       <Main>
+        <PhaseBanner level="alpha">This system is in development</PhaseBanner>
         <Breadcrumbs>All Victims</Breadcrumbs>
         <Heading size="LARGE">All Victims</Heading>
         <GridRow>
           <GridCol setWidth="two-thirds">
-            {/* <SearchBox>
-              <SearchBox.Input placeholder="Search GOV.UK" />
-              {/* <SearchBox.Button /> 
-            </SearchBox> */}
+            <SearchBox>
+              <SearchBox.Input placeholder="Search victims" />
+              <SearchBox.Button />
+            </SearchBox>
           </GridCol>
         </GridRow>
         <Table
           head={
             <Table.Row>
-              <Table.Cell>Full Name</Table.Cell>
+              <Table.CellHeader>Full Name</Table.CellHeader>
             </Table.Row>
           }
           body={victims.map((victim) => {
             return (
               <Table.Row>
-                <Table.Cell>{victim.fullName}</Table.Cell>
+                <Table.Cell>
+                  <Link href="#">{victim.fullName}</Link>
+                </Table.Cell>
               </Table.Row>
             );
           })}
         />
+        <Footer />
       </Main>
     </>
   );
